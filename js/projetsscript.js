bc84be30c55faebab4a0f0723e1f7deaf0dd80d9
RaphB <raphael.bres@etu.univ-grenoble-alpes.fr>
2026-06-23 20:55:53 +0200

    Modifs portfolio

--- js/projetsscript.js
+++ js/projetsscript.js
@@ -1,10 +1,25 @@
 new Swiper('.swiper', {
 
   loop: true,
+  spaceBetween: 30,
+  slidesPerView: 1,
+  centeredSlides: false,
+  grabCursor: true,
+
+  // Nombre de cartes visibles selon la largeur d'écran
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 
   // Navigation arrows
